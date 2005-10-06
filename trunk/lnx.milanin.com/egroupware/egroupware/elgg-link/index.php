2f00c65130717505a4e5e453f89d9d4a222faa77
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2005-10-06 16:32:21 +0000

    first skeleton of an elgg link to profile

---
+++ trunk/lnx.milanin.com/egroupware/egroupware/elgg-link/index.php
@@ -0,0 +1,33 @@
+<?php
+	/**************************************************************************\
+	* eGroupWare SiteMgr - Web Content Management                              *
+	* http://www.egroupware.org                                                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id: index.php,v 1.10.2.1 2004/08/27 18:24:41 ralfbecker Exp $ */
+
+	$GLOBALS['phpgw_info']['flags'] = array
+	(
+		'currentapp' => 'elgg-link',
+		'noheader'   => True,
+		'nonavbar'   => True,
+		'noapi'      => False
+	);
+	$parentdir = dirname(dirname($_SERVER['SCRIPT_FILENAME']));
+	if (file_exists($parentdir.'/header.inc.php'))
+	{
+		include($parentdir.'/header.inc.php');
+	}
+	else
+	{
+		die("You need to make sure the elgg-link app is in the eGroupWare directory.");
+	}
+	$location=$_SERVER['SERVER_NAME'].'/members/profile/index.php?profile_name='.$GLOBALS['phpgw_info']['user']['account_lid'];
+	header("Location: http://$location");
+	
+?>
---
+++ trunk/lnx.milanin.com/egroupware/egroupware/elgg-link/setup/setup.inc.php
@@ -0,0 +1,26 @@
+<?php
+	/**************************************************************************\
+	* eGroupWare SiteMgr - Web Content Management                              *
+	* http://www.egroupware.org                                                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id: setup.inc.php,v 1.8 2004/07/02 22:40:39 ralfbecker Exp $ */
+
+	$setup_info['elgg-link']['name']      = 'elgg-link';
+	$setup_info['elgg-link']['title']     = 'Link to Profiles';
+	$setup_info['elgg-link']['version']   = '0.0.1';
+	$setup_info['elgg-link']['app_order'] = 1;
+	$setup_info['elgg-link']['tables']    = array();
+	$setup_info['elgg-link']['enable']    = 1;
+
+	/* Dependencies for this app to work */
+	$setup_info['elgg-link']['depends'][] = array(
+		'appname'  => 'phpgwapi',
+		'versions' => Array('0.9.14','0.9.15','1.0.0')
+	);
+?>
