546411060545d235898e6859c3d933c25194c6b3
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2005-10-19 11:40:31 +0000

    Profiles skeleton enchachments

--- trunk/lnx.milanin.com/egroupware/egroupware/elgg-link/index.php
+++ trunk/lnx.milanin.com/egroupware/egroupware/elgg-link/index.php
@@ -22,12 +22,33 @@
 	if (file_exists($parentdir.'/header.inc.php'))
 	{
 		include($parentdir.'/header.inc.php');
+		$GLOBALS['phpgw']->common->phpgw_header();
+		echo parse_navbar();
+		$members['online']=Array();
+		
+                
+		foreach ($GLOBALS['phpgw']->session->list_sessions(0,'','session_logintime') as $member){
+                  if (!in_array($member['session_lid'],$members['online'])) $members['online'][]=$member['session_lid'];
+                  //echo print_r($members['online'],1);
+                }
+                
+		echo "<table><tr class=divSideboxEntry><th colspan=3>".lang("Members")." ".lang("Registered")."</th></tr>\n";
+		$members['registered']=$GLOBALS['phpgw']->accounts->get_list('accounts');
+		foreach ($members['registered'] as $member){
+                  if ($member['account_status'] == "A") {
+                    echo "<tr class=divSideboxEntry><td><pre>".print_r($member,1)."</pre></td><td>Link to profile</td><td>";
+                    echo (in_array($member['account_lid'],$members['online'])) ? lang("Online") : lang("Offline");
+                    echo "</td></tr>";
+                  }
+                }
+                echo "</table>";
+		$GLOBALS['phpgw']->common->phpgw_footer();
 	}
 	else
 	{
 		die("You need to make sure the elgg-link app is in the eGroupWare directory.");
 	}
 	$location=$_SERVER['SERVER_NAME'].'/members/'.$GLOBALS['phpgw_info']['user']['account_lid'];
-	header("Location: http://$location");
+	//header("Location: http://$location");
 	
 ?>
