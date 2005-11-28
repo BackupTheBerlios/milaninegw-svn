dfb74c4a74e009c28bd4c36133acf1094dc212b2
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2005-11-28 11:12:29 +0000

    fixed a bug that caused /members/profile to show some shit, now it show the profile of the currently logged in user

--- trunk/lnx.milanin.com/egroupware/members/units/profile/function_init.php
+++ trunk/lnx.milanin.com/egroupware/members/units/profile/function_init.php
@@ -5,8 +5,7 @@
 	// ID of profile to view / edit
 
 		global $profile_id;
-	
-		if (isset($_GET['profile_name'])) {
+		if (isset($_GET['profile_name']) && $_GET['profile_name'] != 'profile') {
 			$profile_id = (int) run("users:name_to_id", $_GET['profile_name']);
 		} else if (isset($_GET['profile_id'])) {
 			$profile_id = (int) $_GET['profile_id'];
@@ -14,6 +13,8 @@
 			$profile_id = (int) $_POST['profileid'];
 		} else if (isset($_SESSION['userid'])) {
 			$profile_id = (int) $_SESSION['userid'];
+                } else if (isset($_SESSION['username'])) {
+			$profile_id = (int) run("users:name_to_id", $_SESSION['username']);
 		} else {
 			$profile_id = -1;
 		}
