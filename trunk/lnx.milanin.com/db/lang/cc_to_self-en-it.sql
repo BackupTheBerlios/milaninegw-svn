19a4c4da812deeb80783fa7e74f8dafede5b5915
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2006-01-29 10:50:49 +0000

    Created a folder to store sql lang updates

---
+++ trunk/lnx.milanin.com/db/lang/cc_to_self-en-it.sql
@@ -0,0 +1,6 @@
+INSERT INTO `phpgw_lang` ( `lang` , `app_name` , `message_id` , `content` )
+VALUES (
+'en', 'messenger', 'cc to self', 'Send a copy to yourself'
+), (
+'it', 'messenger', 'cc to self', 'Inviare una copia a lei'
+);
