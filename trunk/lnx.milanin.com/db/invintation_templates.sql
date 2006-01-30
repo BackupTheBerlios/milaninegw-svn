df92ca7409a39880578386dcf3b27612524c35bb
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2006-01-30 13:53:06 +0000

    new invitation templates

--- trunk/lnx.milanin.com/db/invintation_templates.sql
+++ trunk/lnx.milanin.com/db/invintation_templates.sql
@@ -1,5 +1,19 @@
- 
-insert into `templates` (`ident`, `name`, `owner`, `public`) values (7, 'invite_title', 1, 'yes')
-insert into `template_elements` (`name`, `content`, `template_id`) values ('<option>Standart invitation</option>', 'Standart invitation body', 7)
-insert into `template_elements` (`name`, `content`, `template_id`) values ('<option>Invite a friend</option>', 'Invite a friend body', 7)
-insert into `template_elements` (`name`, `content`, `template_id`) values ('<option selected=true>Invite your colleague</option>', 'Invite your colleague body', 7)
+delete FROM `templates` where `ident` > 5 and `ident` < 11;
+delete FROM `template_elements` where `template_id` > 5 and `template_id` < 11;
+insert into  templates (ident, name, owner, public) values(6, 'invitation message template', 1, 'yes');
+insert into `template_elements` (`name`, `content`, `template_id`) values ('language1', 'English',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('language2', 'Italian',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('language3', 'Russian',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('title1:language1', 'en title1:Title1 text',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('title2:language1', 'en title2:Title2 text',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('title3:language1', 'en title3:Title3 text',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('title1:language2', 'it title1:Title1 text',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('title2:language2', 'it title2:Title2 text',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('title1:language3', 'ru title1:Title1 text',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('msg1:title1:language1', 'en msg1:message body1 text english',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('msg2:title2:language1', 'en msg2:message body2 text english',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('msg3:title3:language1', 'en msg3:message body3 text english',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('msg1:title1:language2', 'it msg1:message body1 text italiano',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('msg2:title2:language2', 'it msg2:message body2 text italiano',6);
+insert into `template_elements` (`name`, `content`, `template_id`) values ('msg1:title1:language3', 'ru msg1:message body1 text russian',6);
+
