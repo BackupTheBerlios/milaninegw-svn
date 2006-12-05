acebdfbcd29cd2280b876609ef17dc454d2028e1
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2006-12-05 14:56:30 +0000

    Added also to sitemgr translations table

--- trunk/lnx.milanin.com/db/update20061205.sql
+++ trunk/lnx.milanin.com/db/update20061205.sql
@@ -3,3 +3,20 @@ insert into `phpgw_lang` (lang, app_name, message_id, content) values ('it', 'ne
 
 insert into `phpgw_lang` (lang, app_name, message_id, content) values ('en', 'news_admin', 'home page', 'Home page');
 insert into `phpgw_lang` (lang, app_name, message_id, content) values ('it', 'news_admin', 'home page', 'Home page');
+
+INSERT INTO `phpgw_lang` ( lang, app_name, message_id, content )
+VALUES (
+'en', 'sitemgr', 'permanent link', 'Permanent link'
+);# Affected rows: 1
+INSERT INTO `phpgw_lang` ( lang, app_name, message_id, content )
+VALUES (
+'it', 'sitemgr', 'permanent link', 'Link permanente'
+);# Affected rows: 1
+INSERT INTO `phpgw_lang` ( lang, app_name, message_id, content )
+VALUES (
+'en', 'sitemgr', 'home page', 'Home page'
+);# Affected rows: 1
+INSERT INTO `phpgw_lang` ( lang, app_name, message_id, content )
+VALUES (
+'it', 'sitemgr', 'home page', 'Home page'
+);# Affected rows: 1
