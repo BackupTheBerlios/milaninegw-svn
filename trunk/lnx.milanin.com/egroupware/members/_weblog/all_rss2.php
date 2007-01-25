d9ac0be33866c11533662ee73d827b41d1cafb1a
gfdsa <gfdsa@cda47673-9b00-0410-bf0c-a91f978f6119>
2007-01-25 14:26:24 +0000

    Added members icons

--- trunk/lnx.milanin.com/egroupware/members/_weblog/all_rss2.php
+++ trunk/lnx.milanin.com/egroupware/members/_weblog/all_rss2.php
@@ -36,16 +36,17 @@ END;
     <link>$mainurl</link>
 END;
 				if (!isset($_REQUEST['tag'])) {
-					$entries = db_query("select posts.*, users.username , users.name from ".tbl_prefix."weblog_posts posts left join ".tbl_prefix."users users on posts.owner=users.ident where access = 'PUBLIC' order by posted desc limit 30");
+					$entries = db_query("select posts.*, users.username , users.name,icons.filename from ".tbl_prefix."weblog_posts posts left join ".tbl_prefix."users users on posts.owner=users.ident left join ".tbl_prefix."icons icons on icons.ident=users.icon where access = 'PUBLIC' order by posted desc limit 30");
 				} else {
 					$tag = addslashes($_REQUEST['tag']);
-					$entries = db_query("select ".tbl_prefix."weblog_posts.* from ".tbl_prefix."tags left join ".tbl_prefix."weblog_posts.on ".tbl_prefix."weblog_posts.ident = tags.ref where  ".tbl_prefix."weblog_posts.access = 'PUBLIC' and tags.tag = '$tag' and tags.tagtype = 'weblog' order by ".tbl_prefix."weblog_posts.posted desc limit 10");
+					$entries = db_query("select ".tbl_prefix."weblog_posts.* from ".tbl_prefix."tags left join ".tbl_prefix."weblog_posts posts on ".tbl_prefix."weblog_posts.ident = tags.ref where  ".tbl_prefix."weblog_posts.access = 'PUBLIC' and tags.tag = '$tag' and tags.tagtype = 'weblog' order by ".tbl_prefix."weblog_posts.posted desc limit 10");
 				}
 				if (sizeof($entries) > 0) {
 					foreach($entries as $entry) {
 						$title = htmlentities(stripslashes($entry->name.' :: '.$entry->title));
 						$link = url .$entry->username . "/weblog/" . $entry->ident . ".html";
-						$body = htmlentities(run("weblogs:text:process",stripslashes($entry->body)));
+                                                $body_header="<div style=\"margin:5px;float:left\"><img src=\"".url."_icons/data/".$entry->filename."\"/></div>";
+						$body = htmlentities($body_header.run("weblogs:text:process",stripslashes($entry->body)));
 						$pubdate = gmdate("D, d M Y H:i:s T", $entry->posted);
 						$keywords = db_query("select * from ".tbl_prefix."tags where tagtype = 'weblog' and ref = '".$entry->ident."'");
 						$keywordtags = "";
